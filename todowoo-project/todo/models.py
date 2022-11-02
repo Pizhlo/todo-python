f85a343669ce354fb154328b759bb83ba3d29df9
pizhlo <zaush@bk.ru>
2022-11-02 14:37:42 +0300

    created Todo model

--- todowoo-project/todo/models.py
+++ todowoo-project/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Название')
+    memo = models.TextField(blank=True, verbose_name='Описание')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_completed = models.DateTimeField(null=True, verbose_name='Дата выполнения', blank=True)
+    is_important = models.BooleanField(default=False, verbose_name='Пометить как важное')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
